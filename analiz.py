2c349d6a12055fc27a1a229444405f8448bb1490
mertfsmal <mertfsmal@gmail.com>
2021-08-18 21:36:59 +0300

    kaynaklar, ingilizce(), kelimeler(), if startswith, ModuleNotFoundError

--- analiz.py
+++ analiz.py
@@ -35,6 +35,8 @@ def mertfsmal():
     print('Repository: https://github.com/mertfsmal/kelime-analiz')
     time.sleep(0.5)
     print('Discord: mrt#0269')
+    time.sleep(0.5)
+    print('Kaynaklar: www.kelimeler.net, www.etimolojiturkce.com')
 
 def kucuk(kucuklestir):
     kucuklestir = kucuklestir.replace("İ", "i")
@@ -42,13 +44,24 @@ def kucuk(kucuklestir):
     kucuklestir = kucuklestir.lower()
     return kucuklestir
 
+def ingilizce(ingilizcelestir):
+    ingilizcelestir = ingilizcelestir.replace("İ", "i")
+    ingilizcelestir = ingilizcelestir.lower()
+    ingilizcelestir = ingilizcelestir.replace("ı", "i")
+    ingilizcelestir = ingilizcelestir.replace("ş", "s")
+    ingilizcelestir = ingilizcelestir.replace("ç", "c")
+    ingilizcelestir = ingilizcelestir.replace("ğ", "g")
+    ingilizcelestir = ingilizcelestir.replace("ö", "o")
+    ingilizcelestir = ingilizcelestir.replace("ü", "u")
+    return ingilizcelestir
+
 def etimoloji(aranacak):
     res = requests.get(f'https://www.etimolojiturkce.com/kelime/{aranacak}')
     soup = bs4.BeautifulSoup(res.text, "html.parser")
     if soup.title.string == '404 Not Found':
         print(f'"{aranacak}" kelimesinin etimolojisi bulunamadı.')
     else:
-        print(f'"{aranacak}" kelimeinin etimolojisi bulundu.')
+        print(f'"{aranacak}" kelimesinin etimolojisi bulundu.')
 
 def harfListe(yazdir, kelime, liste,):
     say=0
@@ -57,27 +70,53 @@ def harfListe(yazdir, kelime, liste,):
             say = say + 1
     print(yazdir, say)
 
+def kelimeler(aranacak):
+    if len(aranacak)-1 >= 1:
+        aranacak=ingilizce(aranacak.replace('.', ''))
+        if len(aranacak.split()) >= 2:
+            #input(f'harfimiz: {aranacak.split()[0]}, komudumuz: {aranacak.split()[1]}')
+            print()
+            if aranacak.split()[1] == 'baslayan' or 'biten':
+                if aranacak.split()[1] == 'baslayan':
+                    res = requests.get(f'https://kelimeler.net/{aranacak.split()[0]}-ile-baslayan-kelimeler')
+                    soup = bs4.BeautifulSoup(res.text, "html.parser")
+                    aciklama = soup.find('p').get_text().split()
+                    print(aciklama[0], aciklama[1], aciklama[2], aciklama[3], aciklama[4], aciklama[5],)
+                if aranacak.split()[1] == 'biten':
+                    res = requests.get(f'https://kelimeler.net/{aranacak.split()[0]}-ile-biten-kelimeler')
+                    soup = bs4.BeautifulSoup(res.text, "html.parser")
+                    print(soup.title.string)
+            else:
+                print('Yanlış kullanım. Komudunuz "baslayan" veya "biten" olmalı')
+        else:
+            print('Yanlış kullanım. Yazdığınız komut 2 kelime veya 2 kelimeden fazla olmalı.')
+    else:
+        print(' "." ile başlayıp devamını getirmedin.')
+
 def uygulama():
     while True:
         time.sleep(0.6)
         print()
         analiz=kucuk(input('Analiz edilecek kelimeyi girin. '))
-        print('-')
-        print('Genel bilgi:')
-        print("Kelime sayısı:",len(analiz.split()))
-        print('Toplam harf:', len(analiz)-len(analiz.split())+1)
-        for kelime in analiz.split():
-            time.sleep(0.6)
+        if analiz.startswith('.'):
+            kelimeler(analiz)
+        else:
             print('-')
-            print(f'{kelime} kelimesinin analizi:')
-            harfListe('ünlü ve hece sayısı:', kelime, unlu)
-            harfListe('Kalın ünlü harflerin sayısı:', kelime, kalinUnlu)
-            harfListe('İnce ünlü harflerin sayısı:', kelime, inceUnlu)
-            harfListe('Ünsüz harflerin sayısı:', kelime, unsuz)
-            harfListe('Yumuşak ünsüz harflerin sayısı:', kelime, yumusakUnsuz)
-            harfListe('Sert ünsüz harflerin sayısı:', kelime, sertUnsuz)
-            harfListe('Türkçe harflerin sayısı:', kelime, turkce)
-            etimoloji(kelime)
+            print('Genel bilgi:')
+            print("Kelime sayısı:",len(analiz.split()))
+            print('Toplam harf:', len(analiz)-len(analiz.split())+1)
+            for kelime in analiz.split():
+                time.sleep(0.6)
+                print('-')
+                print(f'{kelime} kelimesinin analizi:')
+                harfListe('ünlü ve hece sayısı:', kelime, unlu)
+                harfListe('Kalın ünlü harflerin sayısı:', kelime, kalinUnlu)
+                harfListe('İnce ünlü harflerin sayısı:', kelime, inceUnlu)
+                harfListe('Ünsüz harflerin sayısı:', kelime, unsuz)
+                harfListe('Yumuşak ünsüz harflerin sayısı:', kelime, yumusakUnsuz)
+                harfListe('Sert ünsüz harflerin sayısı:', kelime, sertUnsuz)
+                harfListe('Türkçe harflerin sayısı:', kelime, turkce)
+                etimoloji(kelime)
 
 if __name__ == '__main__':
     try:
@@ -86,6 +125,13 @@ if __name__ == '__main__':
         import bs4
         mertfsmal()
         uygulama()
+    except ModuleNotFoundError:
+        import pip
+        pip.main(['install requests'])
+        pip.main(['install bs4'])
+    #except HTTPSConnectionPool:
+    #    print('Siteye bağlanılamadı.')
+    #    input()
     except Exception as e:
         m=input('Hatayı görüntülemek için "e" yazın. ')
         if m=='e':
